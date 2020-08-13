59bb0a44f3eb9e2c025c65be00927e0af39e180c
Martini024 <zhao435021640@gmail.com>
2020-08-13 11:03:19 +0800

    clean up

--- ClientApp/src/page/department/dept-head/MaintainDeptForm.js
+++ ClientApp/src/page/department/dept-head/MaintainDeptForm.js
@@ -1,60 +1,60 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
+
 import React from "react";
 
 export default function MaintainDeptForm() {
 	return (
-<<<<<<< HEAD
-		<div>
-			<h1>Maintain Department</h1>
-
-=======
 		<div className="">
 			<h3>Maintain Department</h3>
 			<Form>
-					<Form.Group as={Row}>
-						<Form.Label as="legend" className="" column sm={2}>Collection Point : </Form.Label>
+				<Form.Group as={Row}>
+					<Form.Label as="legend" className="" column sm={2}>
+						Collection Point :{" "}
+					</Form.Label>
 
-						<Col sm={10}>
-							<Form.Check
-								type="radio"
-								label="Stationery Store - Administration Building (9:30 AM)"
-								value="STORE"
-								className=""
-							/>
-							<Form.Check
-								type="radio"
-								label="Management School (11:00 AM)"
-								value="DEPARTMENT"
-								className=""
-							/>
-							<Form.Check
-								type="radio"
-								label="Medical School (9:30 AM)"
-								value="DEPARTMENT"
-								className=""
-							/>
-							<Form.Check
-								type="radio"
-								label="Engineering School (9:30 AM)"
-								value="DEPARTMENT"
-								className=""
-							/>
-							<Form.Check
-								type="radio"
-								label="Science School (9:30 AM)"
-								value="DEPARTMENT"
-								className=""
-							/>
-							<Form.Check
-								type="radio"
-								label="University Hospital (11:00 AM)"
-								value="DEPARTMENT"
-								className=""
-							/>
-						</Col>
-					</Form.Group>
+					<Col sm={10}>
+						<Form.Check
+							type="radio"
+							label="Stationery Store - Administration Building (9:30 AM)"
+							value="STORE"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Management School (11:00 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Medical School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Engineering School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Science School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="University Hospital (11:00 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+					</Col>
+				</Form.Group>
 				<Form.Group as={Row} controlId="nameOrEmail">
-					<Form.Label className="" column sm={2}>New Representative Name : </Form.Label>
+					<Form.Label className="" column sm={2}>
+						New Representative Name :{" "}
+					</Form.Label>
 					<Col sm={10}>
 						<Form.Control as="select">
 							<option>1</option>
@@ -67,14 +67,16 @@ export default function MaintainDeptForm() {
 				</Form.Group>
 				<Form.Group className="justify-content-center">
 					<Col sm={1}>
-						<Button type="button" className="btn btn-success floatRight" onClick={handleSubmit}>
+						<Button
+							type="button"
+							className="btn btn-success floatRight"
+							onClick={handleSubmit}
+						>
 							Update
-                        </Button>
+						</Button>
 					</Col>
 				</Form.Group>
 			</Form>
->>>>>>> Stashed changes
->>>>>>> toetoe_maintainDept
 		</div>
 	);
 }
