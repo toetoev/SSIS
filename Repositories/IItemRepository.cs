8f59b3299b1785448a5df74da0b1c0189cbe38f2
Martini024 <zhao435021640@gmail.com>
2020-08-13 20:13:13 +0800

    finish create requisition

--- Repositories/IItemRepository.cs
+++ Repositories/IItemRepository.cs
@@ -1,27 +1,15 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using SSIS.Models;
-=======
-﻿using SSIS.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
->>>>>>> b3e833763d8bd0c17d57d81163798f2fb9a91df8
+using SSIS.Models;
 
 namespace SSIS.Repositories
 {
-    public interface IItemRepository
-    {
-        Task<Item> GetItemById(Item item);
-<<<<<<< HEAD
-        Task<List<Item>> GetAll();
-        Task<bool> ItemExist(Guid itemId);
-=======
-        Task<List<Item>> GetAllItemsFromRepository();
->>>>>>> b3e833763d8bd0c17d57d81163798f2fb9a91df8
-    }
+        public interface IItemRepository
+        {
+                Task<Item> GetItemById(Item item);
+                Task<List<Item>> GetAll();
+                Task<bool> ItemExist(Guid itemId);
+        }
 }
--- Repositories/ItemRepository.cs
+++ Repositories/ItemRepository.cs
@@ -1,20 +1,10 @@
-<<<<<<< HEAD
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SSIS.Databases;
 using SSIS.Models;
-=======
-﻿using Microsoft.EntityFrameworkCore;
-using SSIS.Databases;
-using SSIS.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
->>>>>>> b3e833763d8bd0c17d57d81163798f2fb9a91df8
 
 namespace SSIS.Repositories
 {
@@ -26,11 +16,7 @@ namespace SSIS.Repositories
         {
             _dbContext = dbContext;
         }
-<<<<<<< HEAD
         public async Task<List<Item>> GetAll()
-=======
-        public async Task<List<Item>> GetAllItemsFromRepository()
->>>>>>> b3e833763d8bd0c17d57d81163798f2fb9a91df8
         {
             return await _dbContext.Items.ToListAsync();
         }
@@ -39,7 +25,6 @@ namespace SSIS.Repositories
         {
             return await _dbContext.Items.Where(i => i.Id == item.Id).FirstOrDefaultAsync();
         }
-<<<<<<< HEAD
 
         public async Task<bool> ItemExist(Guid itemId)
         {
@@ -47,7 +32,3 @@ namespace SSIS.Repositories
         }
     }
 }
-=======
-    }
-}
->>>>>>> b3e833763d8bd0c17d57d81163798f2fb9a91df8
