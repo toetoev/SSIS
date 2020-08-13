a82ee9510516a794dae61d29a13274fddf5f27fa
shwesin <shwesin79999@gmail.com>
2020-08-13 12:44:12 +0800

    updated maintain Department

--- ClientApp/src/page/department/dept-head/MaintainDeptForm.js
+++ ClientApp/src/page/department/dept-head/MaintainDeptForm.js
@@ -53,23 +53,73 @@ export default function MaintainDeptForm() {
 						</Col>
 					</Form.Group>
 				</fieldset>
-				<Form.Group as={Row} controlId="nameOrEmail">
-					<Form.Label className="" column sm={2}>
-						New Representative Name :{" "}
+		<div className="col-lg-10 mt-3">
+			<h4>Maintain Department</h4>
+			<Form className="mt-4">
+				<Form.Group as={Row} sm={12}>
+					<Form.Label as="legend" className="" column sm={3}>
+						Collection Point :
 					</Form.Label>
-					<Col sm={10}>
+					<Col sm={8}>
+						<Form.Check
+							type="radio"
+							label="Stationery Store - Administration Building (9:30 AM)"
+							value="STORE"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Management School (11:00 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Medical School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Engineering School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="Science School (9:30 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+						<Form.Check
+							type="radio"
+							label="University Hospital (11:00 AM)"
+							value="DEPARTMENT"
+							className=""
+						/>
+					</Col>
+				</Form.Group>
+>>>>>>> Stashed changes
+				<Form.Group as={Row} controlId="nameOrEmail">
+					<Form.Label className="" column sm={3}>
+						New Representative Name :
+					</Form.Label>
+					<Col sm={4}>
 						<Form.Control as="select">
-							<option>1</option>
+							<option>Martini</option>
 							<option>2</option>
-							<option>3</option>
-							<option>4</option>
-							<option>5</option>
 						</Form.Control>
 					</Col>
 				</Form.Group>
 				<Form.Group className="justify-content-center">
 					<Col sm={1}>
 						<Button type="button" className="btn btn-success floatRight">
+=======
+					<Col sm={7}>
+						<Button
+							type="button"
+							className="btn btn-success float-right"
+						>
 							Update
 						</Button>
 					</Col>
