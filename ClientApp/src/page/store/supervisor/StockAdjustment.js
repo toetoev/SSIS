0d6706b810787d18b7d95bc634cf2f8a63b02f60
shwesin <shwesin79999@gmail.com>
2020-08-17 21:24:27 +0800

    update design

--- ClientApp/src/page/store/supervisor/StockAdjustment.js
+++ ClientApp/src/page/store/supervisor/StockAdjustment.js
@@ -1,8 +1,19 @@
-import { Button, Input, Space, Table } from "antd";
+import { Button, Col, Form, Input, Modal, Row, Select, Space, Table } from "antd";
 import React, { useState } from "react";
 
 export default function StockAdjustment() {
-	const [dataSource, setDataSource] = useState([]);
+	//const [dataSource, setDataSource] = useState([]);
+	const dataSource = [
+		{
+			key: "1",
+			submittedOn: "09-09-2020",
+			submittedBy: "09-09-2020",
+			authorizedBy: "Meka",
+			status: "Applied",
+			action: "action",
+		},
+	];
+
 	const columns = [
 		{
 			title: "Submitted On",
@@ -28,19 +39,7 @@ export default function StockAdjustment() {
 			title: "Action",
 			dataIndex: "action",
 			key: "action",
-			render: () => (
-				<Space>
-					<Button>
-						<a>View</a>
-					</Button>
-					<Button type="primary">
-						<a>Edit</a>
-					</Button>
-					<Button type="danger">
-						<a>Delete</a>
-					</Button>
-				</Space>
-			),
+			render: AdjustmentVoucher,
 		},
 	];
 
@@ -59,3 +58,93 @@ export default function StockAdjustment() {
 		</>
 	);
 }
+
+const AdjustmentVoucher = () => {
+	const dataSource = [
+		{
+			key: "1",
+			category: "File",
+			description: "File-Brown w/o Logo",
+			quantityAdjusted: "-6",
+			reason: "Broken Item",
+		},
+	];
+	const columns = [
+		{
+			title: "Item Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Item Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "Quantity Adjusted",
+			dataIndex: "quantityAdjusted",
+			key: "quantityAdjusted",
+		},
+		{
+			title: "Reason",
+			dataIndex: "reason",
+			key: "reason",
+		},
+	];
+
+	const [visible, setVisible] = useState(false);
+	//const [status, setStatus] = useState("PENDING_COLLECTION");
+
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+
+	return (
+		<div>
+			<Space>
+				<Button>
+					<a onClick={showModal}>View</a>
+				</Button>
+				<Button type="primary">
+					<a>Edit</a>
+				</Button>
+				<Button type="danger">
+					<a>Delete</a>
+				</Button>
+			</Space>
+			<Modal
+				title="Adjustment Voucher"
+				visible={visible}
+				onCancel={handleCancel}
+				footer={null}
+			>
+				<p>Submitted By : </p>
+				<p>Date Submitted : </p>
+				<p>Issued On : </p>
+
+				<Table columns={columns} dataSource={dataSource} />
+
+				<Row>
+					<Col
+						span={24}
+						style={{
+							textAlign: 'right',
+						}}
+					>
+						<Space>
+							<Button type="danger">
+								<a>Reject</a>
+							</Button>
+							<Button type="primary">
+								<a>Issue</a>
+							</Button>
+						</Space>
+					</Col>
+				</Row>
+			</Modal>
+		</div>
+	);
+};
