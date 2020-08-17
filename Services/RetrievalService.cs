69aee5ea73de21cc87adeda355095bbc0850b257
Martini024 <zhao435021640@gmail.com>
2020-08-17 11:05:12 +0800

    online approved requisition can be added to retrieval

--- Services/RetrievalService.cs
+++ Services/RetrievalService.cs
@@ -29,7 +29,7 @@ namespace SSIS.Services
             foreach (var requisitionId in requisitionIds)
             {
                 Requisition requisition = await _requisitionRepository.GetRequisitionById(requisitionId);
-                if (requisition != null)
+                if (requisition != null && requisition.Status == RequisitionStatus.APPROVED)
                 {
                     requisition.Status = RequisitionStatus.PROCESSING_RETRIEVAL;
                     requisition.RetrievalId = retrievalId;
