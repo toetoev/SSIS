31c34c9c73d753316fb0d611e143b301f2a452b0
Martini024 <zhao435021640@gmail.com>
2020-08-17 17:21:12 +0800

    change totitlecase

--- ClientApp/src/util/toTitleCase.js
+++ ClientApp/src/util/toTitleCase.js
@@ -1,8 +1,10 @@
 const toTitleCase = (string) => {
-	var sentence = string.toLowerCase().split(" ");
-	for (var i = 0; i < sentence.length; i++) {
-		sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+	var sentence = string.toLowerCase().split("_");
+	for (var i = 0; i < sentence.length - 1; i++) {
+		sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1) + " ";
 	}
+	sentence[sentence.length - 1] =
+		sentence[sentence.length - 1][0].toUpperCase() + sentence[sentence.length - 1].slice(1);
 	return sentence;
 };
 
