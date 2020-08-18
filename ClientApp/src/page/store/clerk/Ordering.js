44b168a38bcb441402242c2097a96460b13fac25
Martini024 <zhao435021640@gmail.com>
2020-08-18 17:00:15 +0800

    rename stocklist to ordering

--- ClientApp/src/page/store/clerk/Ordering.js
+++ ClientApp/src/page/store/clerk/Ordering.js
@@ -1,14 +1,23 @@
-import { Button, Form, Input, InputNumber, Modal, Space, Table, Tabs } from "antd";
+import {
+	Button,
+	Col,
+	Form,
+	Input,
+	InputNumber,
+	Modal,
+	Row,
+	Select,
+	Space,
+	Table,
+	Tabs,
+} from "antd";
 import React, { useEffect, useState } from "react";
 
+import Confirm from "../../component/Confirm";
 import axios from "axios";
 
-export default function Ordering() {
-	//const [dataSource, setDataSource] = useState([]);
-	const { Search } = Input;
-	const { TabPane } = Tabs;
-
-	const dataSource = [
+export default function StockList() {
+	const [dataSource, setDataSource] = useState([
 		{
 			key: "1",
 			category: "Clip",
@@ -20,9 +29,12 @@ export default function Ordering() {
 			supplier2: "BETA",
 			supplier3: "GAMA",
 			action: "action",
+			bin: "P39",
+			uom: "Box",
 		},
-	];
-
+	]);
+	const { Search } = Input;
+	const { TabPane } = Tabs;
 	const columns = [
 		{
 			title: "Category",
@@ -72,6 +84,86 @@ export default function Ordering() {
 		},
 	];
 
+	const columns1 = [
+		{
+			title: "Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "Reorder Quantity",
+			dataIndex: "reorderQuantity",
+			key: "reorderQuantity",
+		},
+		{
+			title: "Supplier",
+			dataIndex: "supplier1" || "supplier2" || "supplier3",
+			key: "supplier",
+		},
+		{
+			title: "Action",
+			dataIndex: "action",
+			key: "action",
+			render: (text) => (
+				<Space>
+					<Received text={text}></Received>
+					<Delete
+						dataSource={dataSource}
+						handleDataChange={handleDataChange}
+						text={text}
+					></Delete>
+				</Space>
+			),
+		},
+	];
+
+	const columns2 = [
+		{
+			title: "Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Bin",
+			dataIndex: "bin",
+			key: "bin",
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "UoM",
+			dataIndex: "uom",
+			key: "uom",
+		},
+		{
+			title: "Reorder Level",
+			dataIndex: "reorderLevel",
+			key: "reorderLevel",
+		},
+		{
+			title: "Reorder Quantity",
+			dataIndex: "reorderQuantity",
+			key: "reorderQuantity",
+		},
+		{
+			title: "Stock",
+			dataIndex: "stock",
+			key: "stock",
+		},
+	];
+
+	const handleDataChange = (data) => {
+		setDataSource(data);
+	};
+
 	return (
 		<Space direction="vertical" style={{ width: "100%" }}>
 			<h3>Purchase Orders</h3>
@@ -86,12 +178,29 @@ export default function Ordering() {
 				<TabPane tab="Low-Stock" key="Low-Stock">
 					<Table columns={columns} dataSource={dataSource} />
 				</TabPane>
-				<TabPane tab="Order" key="Order"></TabPane>
-				<TabPane tab="Stocked" key="Stocked"></TabPane>
+				<TabPane tab="Order" key="Order">
+					<Table columns={columns1} dataSource={dataSource} />
+				</TabPane>
+				<TabPane tab="Stocked" key="Stocked">
+					<Table columns={columns2} dataSource={dataSource} />
+				</TabPane>
 			</Tabs>
 		</Space>
 	);
 }
+
+const Delete = ({ dataSource, handleDataChange, text }) => {
+	const handleDelete = () => {
+		Confirm("Are you sure delete the item?", "", () =>
+			handleDataChange(dataSource.filter((val) => val.key !== text.key))
+		);
+	};
+	return (
+		<Button type="danger" onClick={handleDelete}>
+			Delete
+		</Button>
+	);
+};
 
 const Order = ({ dataSource, handleDataChange }) => {
 	const [form] = Form.useForm();
@@ -255,6 +364,103 @@ const Details = ({ dataSource, handleDataChange }) => {
 					dataSource={dataSource}
 					pagination={false}
 				/>
+			</Modal>
+		</>
+	);
+};
+
+const Received = ({ dataSource, handleDataChange }) => {
+	const [form] = Form.useForm();
+	const [item, setItem] = useState("");
+	const [quantity, setQuantity] = useState(0);
+	const [visible, setVisible] = useState(false);
+	const [itemOptions, setItemOptions] = useState([]);
+
+	const showModal = () => {
+		setVisible(true);
+	};
+
+	const handleSubmit = () => {
+		form.validateFields()
+			.then((val) => {
+				if (dataSource.find((val) => val.key === item)) {
+					handleDataChange(
+						dataSource.map((val) => {
+							if (val.key === item) {
+								val.quantity = val.quantity + quantity;
+							}
+							return val;
+						})
+					);
+				} else {
+					handleDataChange([
+						...dataSource,
+						{
+							key: item,
+							description: itemOptions.find((val) => val.value === item).label,
+							quantity: quantity,
+						},
+					]);
+				}
+				setVisible(false);
+			})
+			.catch((err) => {});
+	};
+
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+
+	const onValuesChange = (val) => {
+		if (val.quantity) setQuantity(Number(val.quantity));
+		if (val.item) setItem(val.item);
+	};
+
+	useEffect(() => {
+		axios
+			.get("https://localhost:5001/api/item")
+			.then((res) => {
+				const result = res.data;
+				if (result.success) {
+					setItemOptions(
+						result.data.reduce((options, item) => {
+							return [...options, { label: item.description, value: item.id }];
+						}, [])
+					);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+
+	return (
+		<>
+			<Button type="primary" onClick={showModal}>
+				Received
+			</Button>
+			<Modal
+				title="Purchase Orders"
+				visible={visible}
+				onOk={handleSubmit}
+				onCancel={handleCancel}
+				footer={[
+					<Button key="cancel" onClick={handleCancel}>
+						Cancel
+					</Button>,
+					<Button key="submit" type="primary" onClick={handleSubmit}>
+						Submit
+					</Button>,
+				]}
+			>
+				<Form form={form} layout="vertical" onValuesChange={onValuesChange}>
+					<Form.Item name="item" label="Item Description : ">
+						<Select options={itemOptions} style={{ width: "100%" }}></Select>
+					</Form.Item>
+					<Form.Item name="quantity" label="Quantity : ">
+						<Input type="number" placeholder="0" />
+					</Form.Item>
+				</Form>
 			</Modal>
 		</>
 	);
