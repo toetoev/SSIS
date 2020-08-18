f410027e57ed5f88f5c2cfee7affc26f70e54907
Pranammeka <pranammeka@gmail.com>
2020-08-18 17:39:10 +0800

    refactor tab table to component

--- ClientApp/src/page/store/clerk/RequisitionList.js
+++ ClientApp/src/page/store/clerk/RequisitionList.js
@@ -1,5 +1,17 @@
-import { Button, Col, Form, Input, Modal, Row, Select, Space, Table, InputNumber, Checkbox, Tabs } from "antd";
-import React, { useEffect, useState } from "react";
+import {
+	Button,
+	Checkbox,
+	Col,
+	Descriptions,
+	Form,
+	Input,
+	Modal,
+	Row,
+	Space,
+	Table,
+	Tabs,
+} from "antd";
+import { default as React, useEffect, useState } from "react";
 
 import axios from "axios";
 
@@ -44,60 +56,106 @@ export default function RequisitionList() {
 			title: "Details",
 			dataIndex: "action",
 			key: "action",
-			render: () => (
-				<Button>View</Button>
-			),
+			render: () => <ViewRequisition />,
 		},
 		{
 			title: "Select",
 			dataIndex: "select",
 			key: "select",
-			render: () => (
-				<Checkbox />
-			),
+			render: () => <Checkbox />,
 		},
 	];
 
 	return (
 		<Space direction="vertical" style={{ width: "100%" }}>
 			<h3>Stock Adjustment</h3>
-			<Row
-				justify="space-between"
-				style={{ float: "right" }}
-			>
+			<Row justify="space-between" style={{ float: "right" }}>
 				<Col>
 					<Space>
 						<Search
 							placeholder="input search text"
-							onSearch={value => console.log(value)}
+							onSearch={(value) => console.log(value)}
 							style={{ width: 200 }}
 						/>
-						
-						<Button type="primary">Add</Button>
+
+						<Button type="primary">Add To Retrieval</Button>
 					</Space>
 				</Col>
 			</Row>
 
 			<Tabs defaultActiveKey="To-Do" type="card">
 				<TabPane tab="To-Do" key="To-Do">
-					<Table
-						columns={columns}
-						dataSource={dataSource}
-					/>
+					<Table columns={columns} dataSource={dataSource} />
 				</TabPane>
 				<TabPane tab="Retrieval" key="Retrieval">
 					<Retrieval />
 				</TabPane>
 				<TabPane tab="Disbursement" key="Disbursement">
+					<Disbursement />
 				</TabPane>
 				<TabPane tab="Ready for Delivery" key="RFD">
+					<ReadyForDelivery />
 				</TabPane>
 				<TabPane tab="Completed" key="Completed">
+					<Completed />
 				</TabPane>
 			</Tabs>
 		</Space>
 	);
 }
+
+const ViewRequisition = () => {
+	const itemData = [];
+
+	const reqColumns = [
+		{
+			title: "Item Description",
+			dataIndex: "itemDescription",
+		},
+		{
+			title: "Quantity",
+			dataIndex: "qty",
+		},
+	];
+
+	const [visible, setVisible] = useState(false);
+	const [status, setStatus] = useState("");
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = (e) => {
+		setVisible(false);
+	};
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+	return (
+		<div>
+			<Button type="primary" onClick={showModal}>
+				View
+			</Button>
+			<Modal
+				title="Requisition Details"
+				visible={visible}
+				onCancel={handleCancel}
+				footer={null}
+			>
+				<Descriptions>
+					<Descriptions.Item label="Collection Point">Collection Point</Descriptions.Item>
+				</Descriptions>
+				<Descriptions>
+					<Descriptions.Item label="Requested Items"></Descriptions.Item>
+				</Descriptions>
+				<Table
+					dataSource={itemData}
+					columns={reqColumns}
+					pagination={false}
+					scroll={{ y: 100 }}
+				/>
+			</Modal>
+		</div>
+	);
+};
 
 const Retrieval = () => {
 	const [form] = Form.useForm();
@@ -127,12 +185,7 @@ const Retrieval = () => {
 			key: "action",
 			render: () => (
 				<Space>
-					<Button>
-						<a>View</a>
-					</Button>
-					<Button type="danger">
-						<a>Delete</a>
-					</Button>
+					<MaintainRetrieval />
 				</Space>
 			),
 		},
@@ -144,7 +197,6 @@ const Retrieval = () => {
 			.then((res) => {
 				const result = res.data;
 				if (result.success) {
-
 				}
 			})
 			.catch(function (error) {
@@ -154,11 +206,309 @@ const Retrieval = () => {
 
 	return (
 		<>
-			<Table
-				columns={columns}
-				dataSource={dataSource}
-				pagination={false}
-			/>
+			<Table columns={columns} dataSource={dataSource} pagination={false} />
+		</>
+	);
+};
+
+const MaintainRetrieval = () => {
+	const itemData = [];
+
+	const reqColumns = [
+		{
+			title: "Bin",
+			dataIndex: "bin",
+		},
+		{
+			title: "Item Description",
+			dataIndex: "itemDescription",
+		},
+		{
+			title: "Amount Needed",
+			dataIndex: "needed",
+		},
+		{
+			title: "Retrieved Amount",
+			dataIndex: "retrieved",
+		},
+	];
+
+	const [visible, setVisible] = useState(false);
+	const [status, setStatus] = useState("");
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = (e) => {
+		setVisible(false);
+	};
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+	return (
+		<div>
+			<Button type="primary" onClick={showModal}>
+				View
+			</Button>
+			<Button type="danger">Delete</Button>
+			<Modal
+				title="Requisition Details"
+				visible={visible}
+				onCancel={handleCancel}
+				footer={null}
+			>
+				<Table
+					dataSource={itemData}
+					columns={reqColumns}
+					pagination={false}
+					scroll={{ y: 100 }}
+				/>
+				<Button type="secondary">Print</Button>
+				<Button type="secondary">Save</Button>
+				<Button type="primary">Confirm</Button>
+			</Modal>
+		</div>
+	);
+};
+
+const Disbursement = () => {
+	const [form] = Form.useForm();
+
+	const dataSource = [];
+
+	const columns = [
+		{
+			title: "Retrieved Item",
+			dataIndex: "retrievedItem",
+		},
+		{
+			title: "Amount Retrieved",
+			dataIndex: "amountRetrieved",
+		},
+		{
+			title: "Action",
+			dataIndex: "action",
+			key: "action",
+			render: () => (
+				<Space>
+					<Disburse />
+				</Space>
+			),
+		},
+	];
+
+	const Disburse = () => {
+		const itemData = [];
+
+		const reqColumns = [
+			{
+				title: "Department",
+				dataIndex: "department",
+			},
+			{
+				title: "Requested By",
+				dataIndex: "requestedBy",
+			},
+			{
+				title: "Requested Date",
+				dataIndex: "requestedDate",
+			},
+			{
+				title: "Needed Amount",
+				dataIndex: "neededAmount",
+			},
+			{
+				title: "Actual Amount",
+				dataIndex: "actualAmount",
+			},
+		];
+
+		const [visible, setVisible] = useState(false);
+		const [status, setStatus] = useState("");
+		const showModal = () => {
+			setVisible(true);
+		};
+		const handleOk = (e) => {
+			setVisible(false);
+		};
+		const handleCancel = (e) => {
+			setVisible(false);
+		};
+		return (
+			<div>
+				<Button type="primary" onClick={showModal}>
+					Disburse
+				</Button>
+				<Modal title="" visible={visible} onCancel={handleCancel} footer={null}>
+					<Table
+						dataSource={itemData}
+						columns={reqColumns}
+						pagination={false}
+						scroll={{ y: 100 }}
+					/>
+					<Button type="secondary">Print</Button>
+					<Button type="secondary">Save</Button>
+					<Button type="primary">Confirm</Button>
+				</Modal>
+			</div>
+		);
+	};
+
+	useEffect(() => {
+		axios
+			.get("https://localhost:5001/api/item")
+			.then((res) => {
+				const result = res.data;
+				if (result.success) {
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+
+	return (
+		<>
+			<Table columns={columns} dataSource={dataSource} pagination={false} />
+		</>
+	);
+};
+
+const DisbursementList = () => {
+	const itemData = [];
+
+	const reqColumns = [
+		{
+			title: "Stationary Description",
+			dataIndex: "bin",
+		},
+		{
+			title: "UoM",
+			dataIndex: "itemDescription",
+		},
+		{
+			title: "Amount Needed",
+			dataIndex: "needed",
+		},
+		{
+			title: "Actual Amount",
+			dataIndex: "actual",
+		},
+	];
+
+	const [visible, setVisible] = useState(false);
+	const [status, setStatus] = useState("");
+	const showModal = () => {
+		setVisible(true);
+	};
+	const handleOk = (e) => {
+		setVisible(false);
+	};
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+	return (
+		<div>
+			<Button type="primary" onClick={showModal}>
+				View
+			</Button>
+			<Button type="danger">Delete</Button>
+			<Modal
+				title="Disbursement List"
+				visible={visible}
+				onCancel={handleCancel}
+				footer={null}
+			>
+				<Table
+					dataSource={itemData}
+					columns={reqColumns}
+					pagination={false}
+					scroll={{ y: 100 }}
+				/>
+				<Button type="secondary">Print</Button>
+				<Button type="primary">Update</Button>
+			</Modal>
+		</div>
+	);
+};
+
+const ReadyForDelivery = () => {
+	const [form] = Form.useForm();
+
+	const dataSource = [];
+
+	const columns = [
+		{
+			title: "Department Name",
+			dataIndex: "departmentName",
+		},
+		{
+			title: "Requested By",
+			dataIndex: "requestedBy",
+		},
+		{
+			title: "Requested Date",
+			dataIndex: "requestedDate",
+		},
+		{
+			title: "Collection Point",
+			dataIndex: "collectionPoint",
+		},
+		{
+			title: "Collection Date",
+			dataIndex: "collectionDate",
+		},
+		{
+			title: "Disbursement List",
+			dataIndex: "action",
+			key: "action",
+			render: () => <DisbursementList />,
+		},
+	];
+
+	return (
+		<>
+			<Table columns={columns} dataSource={dataSource} pagination={false} />
+		</>
+	);
+};
+
+const Completed = () => {
+	const [form] = Form.useForm();
+
+	const dataSource = [];
+
+	const columns = [
+		{
+			title: "Department Name",
+			dataIndex: "departmentName",
+		},
+		{
+			title: "Requested By",
+			dataIndex: "requestedBy",
+		},
+		{
+			title: "Requested Date",
+			dataIndex: "requestedDate",
+		},
+		{
+			title: "Collection Point",
+			dataIndex: "collectionPoint",
+		},
+		{
+			title: "Collection Date",
+			dataIndex: "collectionDate",
+		},
+		{
+			title: "Disbursement List",
+			dataIndex: "action",
+			key: "action",
+			render: () => <DisbursementList />,
+		},
+	];
+
+	return (
+		<>
+			<Table columns={columns} dataSource={dataSource} pagination={false} />
 		</>
 	);
 };
--- ClientApp/src/page/store/clerk/StockList.js
+++ ClientApp/src/page/store/clerk/StockList.js
@@ -1,211 +1,286 @@
-import { Button, Input, Space, Table, Row, Col, Form, Select, InputNumber, Modal, Tabs } from "antd";
+import { Button, Form, Input, InputNumber, Modal, Select, Space, Table, Tabs } from "antd";
 import React, { useEffect, useState } from "react";
 
+import Confirm from "../../component/Confirm";
 import axios from "axios";
 
 export default function StockList() {
-    //const [dataSource, setDataSource] = useState([]);
-    const { Search } = Input;
-    const { TabPane } = Tabs;
+	const [dataSource, setDataSource] = useState([
+		{
+			key: "1",
+			category: "Clip",
+			description: "Clip...",
+			reorderQuantity: "2",
+			reorderLevel: "1",
+			stock: "1",
+			supplier1: "ALPHA",
+			supplier2: "BETA",
+			supplier3: "GAMA",
+			action: "action",
+			bin: "P39",
+			uom: "Box",
+		},
+	]);
+	const { Search } = Input;
+	const { TabPane } = Tabs;
+	const columns = [
+		{
+			title: "Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "Reorder Level",
+			dataIndex: "reorderLevel",
+			key: "reorderLevel",
+		},
+		{
+			title: "Reorder Quantity",
+			dataIndex: "reorderQuantity",
+			key: "reorderQuantity",
+		},
+		{
+			title: "Stock",
+			dataIndex: "stock",
+			key: "stock",
+		},
+		{
+			title: "Supplier 1",
+			dataIndex: "supplier1",
+			key: "supplier1",
+		},
+		{
+			title: "Supplier 2",
+			dataIndex: "supplier2",
+			key: "supplier2",
+		},
+		{
+			title: "Supplier 3",
+			dataIndex: "supplier3",
+			key: "supplier3",
+		},
+		{
+			title: "Action",
+			dataIndex: "action",
+			key: "action",
+			render: () => <Order />,
+		},
+	];
 
-    const dataSource = [
-        {
-            key: "1",
-            category: "Clip",
-            description: "Clip...",
-            reorderQuantity: "2",
-            reorderLevel: "1",
-            stock: "1",
-            supplier1: "ALPHA",
-            supplier2: "BETA",
-            supplier3: "GAMA",
-            action: "action",
-        },
-    ];
+	const columns1 = [
+		{
+			title: "Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "Reorder Quantity",
+			dataIndex: "reorderQuantity",
+			key: "reorderQuantity",
+		},
+		{
+			title: "Supplier",
+			dataIndex: "supplier1" || "supplier2" || "supplier3",
+			key: "supplier",
+		},
+		{
+			title: "Action",
+			dataIndex: "action",
+			key: "action",
+			render: (text) => (
+				<Space>
+					<Received text={text}></Received>
+					<Delete
+						dataSource={dataSource}
+						handleDataChange={handleDataChange}
+						text={text}
+					></Delete>
+				</Space>
+			),
+		},
+	];
 
-    const columns = [
-        {
-            title: "Category",
-            dataIndex: "category",
-            key: "category",
-        },
-        {
-            title: "Description",
-            dataIndex: "description",
-            key: "description",
-        },
-        {
-            title: "Reorder Level",
-            dataIndex: "reorderLevel",
-            key: "reorderLevel",
-        },
-        {
-            title: "Reorder Quantity",
-            dataIndex: "reorderQuantity",
-            key: "reorderQuantity",
-        },
-        {
-            title: "Stock",
-            dataIndex: "stock",
-            key: "stock",
-        },
-        {
-            title: "Supplier 1",
-            dataIndex: "supplier1",
-            key: "supplier1",
-        },
-        {
-            title: "Supplier 2",
-            dataIndex: "supplier2",
-            key: "supplier2",
-        },
-        {
-            title: "Supplier 3",
-            dataIndex: "supplier3",
-            key: "supplier3",
-        },
-        {
-            title: "Action",
-            dataIndex: "action",
-            key: "action",
-            render: () => (
-                <Order />
-            ),
-        },
-    ];
+	const columns2 = [
+		{
+			title: "Category",
+			dataIndex: "category",
+			key: "category",
+		},
+		{
+			title: "Bin",
+			dataIndex: "bin",
+			key: "bin",
+		},
+		{
+			title: "Description",
+			dataIndex: "description",
+			key: "description",
+		},
+		{
+			title: "UoM",
+			dataIndex: "uom",
+			key: "uom",
+		},
+		{
+			title: "Reorder Level",
+			dataIndex: "reorderLevel",
+			key: "reorderLevel",
+		},
+		{
+			title: "Reorder Quantity",
+			dataIndex: "reorderQuantity",
+			key: "reorderQuantity",
+		},
+		{
+			title: "Stock",
+			dataIndex: "stock",
+			key: "stock",
+		},
+	];
 
-    return (
-        <Space direction="vertical" style={{ width: "100%" }}>
-            <h3>Purchase Orders</h3>
+	const handleDataChange = (data) => {
+		setDataSource(data);
+	};
 
-            <Search
-                placeholder="input search text"
-                onSearch={value => console.log(value)}
-                style={{ width: 200, float: "right" }}
-            />
+	return (
+		<Space direction="vertical" style={{ width: "100%" }}>
+			<h3>Purchase Orders</h3>
 
-            <Tabs defaultActiveKey="Low-Stock" type="card">
-                <TabPane tab="Low-Stock" key="Low-Stock">
-                    <Table
-                        columns={columns}
-                        dataSource={dataSource}
-                    />
-                </TabPane>
-                <TabPane tab="Order" key="Order">
-                </TabPane>
-                <TabPane tab="Stocked" key="Stocked">
-                </TabPane>
-            </Tabs>
+			<Search
+				placeholder="input search text"
+				onSearch={(value) => console.log(value)}
+				style={{ width: 200, float: "right" }}
+			/>
 
-        </Space>
-    );
+			<Tabs defaultActiveKey="Low-Stock" type="card">
+				<TabPane tab="Low-Stock" key="Low-Stock">
+					<Table columns={columns} dataSource={dataSource} />
+				</TabPane>
+				<TabPane tab="Order" key="Order">
+					<Table columns={columns1} dataSource={dataSource} />
+				</TabPane>
+				<TabPane tab="Stocked" key="Stocked">
+					<Table columns={columns2} dataSource={dataSource} />
+				</TabPane>
+			</Tabs>
+		</Space>
+	);
 }
 
+const Delete = ({ dataSource, handleDataChange, text }) => {
+	const handleDelete = () => {
+		Confirm("Are you sure delete the item?", "", () =>
+			handleDataChange(dataSource.filter((val) => val.key !== text.key))
+		);
+	};
+	return (
+		<Button type="danger" onClick={handleDelete}>
+			Delete
+		</Button>
+	);
+};
+
 const Order = ({ dataSource, handleDataChange }) => {
-    const [form] = Form.useForm();
-    const [visible, setVisible] = useState(false);
+	const [form] = Form.useForm();
+	const [visible, setVisible] = useState(false);
 
-    const orderData = [
-        {
-            key: "1",
-            supplierName: "ALPHA",
-            orderQuantity: "20",
-        },
-    ];
+	const orderData = [
+		{
+			key: "1",
+			supplierName: "ALPHA",
+			orderQuantity: "20",
+		},
+	];
 
-    const columns = [
-        {
-            title: "Supplier Name",
-            dataIndex: "supplierName",
-            key: "supplierName",
-        },
-        {
-            title: "Order Quantity",
-            dataIndex: "orderQuantity",
-            key: "orderQuantity",
-            render :() => (
-                <InputNumber placeholder = "20"/>
-            ),
-        },
-        {
-            title: "Action",
-            dataIndex: "action",
-            key: "action",
-            render: () => (
-                <Details />
-            ),
-        },
-    ];
+	const columns = [
+		{
+			title: "Supplier Name",
+			dataIndex: "supplierName",
+			key: "supplierName",
+		},
+		{
+			title: "Order Quantity",
+			dataIndex: "orderQuantity",
+			key: "orderQuantity",
+			render: () => <InputNumber placeholder="20" />,
+		},
+		{
+			title: "Action",
+			dataIndex: "action",
+			key: "action",
+			render: () => <Details />,
+		},
+	];
 
-    const showModal = () => {
-        setVisible(true);
-    };
+	const showModal = () => {
+		setVisible(true);
+	};
 
-    const handleSubmit = () => {
+	const handleSubmit = () => {};
 
-    };
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
 
-    const handleCancel = (e) => {
-        setVisible(false);
-    };
+	useEffect(() => {
+		axios
+			.get("https://localhost:5001/api/item")
+			.then((res) => {
+				const result = res.data;
+				if (result.success) {
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
 
-    useEffect(() => {
-        axios
-            .get("https://localhost:5001/api/item")
-            .then((res) => {
-                const result = res.data;
-                if (result.success) {
+	const { TextArea } = Input;
 
-                }
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }, []);
-
-    const { TextArea } = Input;
-
-    return (
-        <>
-            <Button type="primary" onClick={showModal}>
-                Order
+	return (
+		<>
+			<Button type="primary" onClick={showModal}>
+				Order
 			</Button>
 
-            <Modal
-                title="Tender Order"
-                visible={visible}
-                onOk={handleSubmit}
-                onCancel={handleCancel}
-                footer={[
-                    <Button key="cancel" onClick={handleCancel}>
-                        Cancel
+			<Modal
+				title="Tender Order"
+				visible={visible}
+				onOk={handleSubmit}
+				onCancel={handleCancel}
+				footer={[
+					<Button key="cancel" onClick={handleCancel}>
+						Cancel
 					</Button>,
-                    <Button key="submit" type="primary" onClick={handleSubmit}>
-                        Submit
+					<Button key="submit" type="primary" onClick={handleSubmit}>
+						Submit
 					</Button>,
-                ]}
-            >
-                <Form
-                    form={form}
-                    layout="vertical"
-                >
-                    <p>Date : </p>
+				]}
+			>
+				<Form form={form} layout="vertical">
+					<p>Date : </p>
 
-                    <Table
-                        columns={columns}
-                        dataSource={orderData}
-                        pagination={false}
-                    />
-                </Form>
-            </Modal>
-        </>
-    );
+					<Table columns={columns} dataSource={orderData} pagination={false} />
+				</Form>
+			</Modal>
+		</>
+	);
 };
 
 const Details = ({ dataSource, handleDataChange }) => {
-    const [visible, setVisible] = useState(false);
+	const [visible, setVisible] = useState(false);
 
-    const columns = [
+	const columns = [
 		{
 			title: "Item Description",
 			dataIndex: "description",
@@ -220,16 +295,14 @@ const Details = ({ dataSource, handleDataChange }) => {
 			title: "Unit Of Measurement",
 			dataIndex: "uom",
 			key: "uom",
-		}
+		},
 	];
 
 	const showModal = () => {
 		setVisible(true);
 	};
 
-	const handleSubmit = () => {
-
-	};
+	const handleSubmit = () => {};
 
 	const handleCancel = (e) => {
 		setVisible(false);
@@ -241,7 +314,6 @@ const Details = ({ dataSource, handleDataChange }) => {
 			.then((res) => {
 				const result = res.data;
 				if (result.success) {
-
 				}
 			})
 			.catch(function (error) {
@@ -251,9 +323,7 @@ const Details = ({ dataSource, handleDataChange }) => {
 
 	return (
 		<>
-			<Button onClick={showModal}>
-				View
-			</Button>
+			<Button onClick={showModal}>View</Button>
 
 			<Modal
 				title="Stationery Details"
@@ -277,11 +347,108 @@ const Details = ({ dataSource, handleDataChange }) => {
 				<p>Address : Blk 1128, #02-1108 Ang Mo Kio Street 62Singapore 622262</p>
 
 				<Table
-					title={() => 'Items : '}
+					title={() => "Items : "}
 					columns={columns}
 					dataSource={dataSource}
 					pagination={false}
 				/>
+			</Modal>
+		</>
+	);
+};
+
+const Received = ({ dataSource, handleDataChange }) => {
+	const [form] = Form.useForm();
+	const [item, setItem] = useState("");
+	const [quantity, setQuantity] = useState(0);
+	const [visible, setVisible] = useState(false);
+	const [itemOptions, setItemOptions] = useState([]);
+
+	const showModal = () => {
+		setVisible(true);
+	};
+
+	const handleSubmit = () => {
+		form.validateFields()
+			.then((val) => {
+				if (dataSource.find((val) => val.key === item)) {
+					handleDataChange(
+						dataSource.map((val) => {
+							if (val.key === item) {
+								val.quantity = val.quantity + quantity;
+							}
+							return val;
+						})
+					);
+				} else {
+					handleDataChange([
+						...dataSource,
+						{
+							key: item,
+							description: itemOptions.find((val) => val.value === item).label,
+							quantity: quantity,
+						},
+					]);
+				}
+				setVisible(false);
+			})
+			.catch((err) => {});
+	};
+
+	const handleCancel = (e) => {
+		setVisible(false);
+	};
+
+	const onValuesChange = (val) => {
+		if (val.quantity) setQuantity(Number(val.quantity));
+		if (val.item) setItem(val.item);
+	};
+
+	useEffect(() => {
+		axios
+			.get("https://localhost:5001/api/item")
+			.then((res) => {
+				const result = res.data;
+				if (result.success) {
+					setItemOptions(
+						result.data.reduce((options, item) => {
+							return [...options, { label: item.description, value: item.id }];
+						}, [])
+					);
+				}
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+
+	return (
+		<>
+			<Button type="primary" onClick={showModal}>
+				Received
+			</Button>
+			<Modal
+				title="Purchase Orders"
+				visible={visible}
+				onOk={handleSubmit}
+				onCancel={handleCancel}
+				footer={[
+					<Button key="cancel" onClick={handleCancel}>
+						Cancel
+					</Button>,
+					<Button key="submit" type="primary" onClick={handleSubmit}>
+						Submit
+					</Button>,
+				]}
+			>
+				<Form form={form} layout="vertical" onValuesChange={onValuesChange}>
+					<Form.Item name="item" label="Item Description : ">
+						<Select options={itemOptions} style={{ width: "100%" }}></Select>
+					</Form.Item>
+					<Form.Item name="quantity" label="Quantity : ">
+						<Input type="number" placeholder="0" />
+					</Form.Item>
+				</Form>
 			</Modal>
 		</>
 	);
