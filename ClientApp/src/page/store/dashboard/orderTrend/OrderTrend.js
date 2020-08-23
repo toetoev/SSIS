2edfaad5f96d0d76869f2198d80797f198652b60
Martini024 <zhao435021640@gmail.com>
2020-08-24 00:51:17 +0800

    todo comment for order trend

--- ClientApp/src/page/store/dashboard/orderTrend/OrderTrend.js
+++ ClientApp/src/page/store/dashboard/orderTrend/OrderTrend.js
@@ -1,7 +1,7 @@
 import { Button, Card, DatePicker, Form, Row, Select } from "antd";
 import React, { useEffect, useState } from "react";
 
-const OrderTrend = () => {
+export default function OrderTrend() {
 	const [dateRange, setDateRange] = useState([]);
 	const [categories, setCategories] = useState([]);
 	const [categoryOptions, setCategoryOptions] = useState([]);
@@ -14,31 +14,40 @@ const OrderTrend = () => {
 		if (val.dateRange) setDateRange(val.dateRange);
 		if (val.categories) setCategories(val.categories);
 	};
-	// TODO: call chart generator
-	const onFinish = () => {};
+	// TODO: call chart generator OrderController GetOrderTrend /startDate/endDate [categories]
+	const onFinish = () => {
+		console.log("Finish");
+	};
 	// TODO: get item categories set to select
 	useEffect(() => {}, []);
 	return (
-		<Row style={{ width: "100%" }}>
-			<Card bordered={false} style={{ width: "100%" }}>
-				<Form layout="inline" onFinish={onFinish} onValuesChange={onValuesChange}>
-					<Form.Item label="Week Range" name="dateRange">
-						<RangePicker picker="month" />
-					</Form.Item>
-					<Form.Item label="Item Category" name="categories">
-						<Select
-							mode="multiple"
-							placeholder="Please select"
-							options={categoryOptions}
-						></Select>
-					</Form.Item>
-					<Form.Item>
-						<Button type="primary">Generate Charts</Button>
-					</Form.Item>
-				</Form>
-			</Card>
-		</Row>
+		<>
+			<Row style={{ width: "100%" }}>
+				<Card bordered={false} style={{ width: "100%" }}>
+					<Form layout="inline" onFinish={onFinish} onValuesChange={onValuesChange}>
+						<Form.Item label="Week Range" name="dateRange">
+							<RangePicker picker="month" />
+						</Form.Item>
+						<Form.Item label="Item Category" name="categories">
+							<Select
+								mode="multiple"
+								style={{ width: "300px" }}
+								placeholder="Please select"
+								options={categoryOptions}
+							></Select>
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit">
+								Generate Charts
+							</Button>
+						</Form.Item>
+					</Form>
+				</Card>
+			</Row>
+			<Row>
+				<Card bordered={false} style={{ width: "50%" }}></Card>
+				<Card bordered={false} style={{ width: "50%" }}></Card>
+			</Row>
+		</>
 	);
-};
-
-export default OrderTrend;
+}
