7c0c4675a06b16ea8a1c6029a246bc9a0f3d0f69
Martini024 <zhao435021640@gmail.com>
2020-08-23 18:41:59 +0800

    modify requisition history, left conditional for delegated

--- ClientApp/src/page/department/employee/RequisitionHistory.js
+++ ClientApp/src/page/department/employee/RequisitionHistory.js
@@ -1,4 +1,4 @@
-import { Button, Descriptions, Modal, Space, Table } from "antd";
+import { Button, Descriptions, Divider, Input, Modal, Space, Table } from "antd";
 import { default as React, useEffect, useState } from "react";
 
 import axios from "axios";
@@ -43,7 +43,7 @@ export default function RequisitionHistory() {
 		{
 			title: "Action",
 			key: "action",
-			render: (text) => <RequisitionModal text={text} />,
+			render: (text) => <RequisitionModal text={text} setLoading={setLoading} />,
 		},
 	];
 
@@ -87,7 +87,7 @@ export default function RequisitionHistory() {
 				setLoading(false);
 				console.log(error);
 			});
-	}, []);
+	}, [loading]);
 
 	return (
 		<Space direction="vertical">
@@ -104,7 +104,7 @@ export default function RequisitionHistory() {
 	);
 }
 
-const RequisitionModal = ({ text }) => {
+const RequisitionModal = ({ text, setLoading }) => {
 	const requisition = text.action;
 	const [dataSource] = useState(
 		requisition.requisitionItems.reduce((rows, requisitionItem) => {
@@ -114,15 +114,18 @@ const RequisitionModal = ({ text }) => {
 					key: requisitionItem.itemId,
 					itemDescription: requisitionItem.item.description,
 					requestedQty: requisitionItem.need,
-					// IMPROVE: conditional render if its -1, render nothing
-					receivedQty: requisitionItem.actual,
-					unfulfilledQty: requisitionItem.need - requisitionItem.actual,
+					receivedQty: requisitionItem.actual === -1 ? null : requisitionItem.actual,
+					unfulfilledQty:
+						requisitionItem.actual === -1
+							? null
+							: requisitionItem.need - requisitionItem.actual,
 				},
 			];
 		}, [])
 	);
-	const [status] = useState(requisition.status);
+	const [status, setStatus] = useState(requisition.status);
 	const [visible, setVisible] = useState(false);
+	const [rejectReason, setRejectReason] = useState("");
 	// IMPROVE: conditional render column based on status
 	const columns = [
 		{
@@ -151,16 +154,74 @@ const RequisitionModal = ({ text }) => {
 		setVisible(false);
 	};
 
+	const handleReview = (reviewResult) => {
+		var data;
+		if (reviewResult === "REJECTED")
+			data = {
+				status: reviewResult,
+				comment: rejectReason,
+			};
+		else data = { status: reviewResult };
+		axios
+			.put("https://localhost:5001/api/requisition/" + text.key, data, {
+				headers: {
+					Authorization: "Bearer " + localStorage.getItem("ACCESS_TOKEN"),
+					"Content-type": "application/json",
+				},
+			})
+			.then((res) => {
+				const result = res.data;
+				if (result.success) {
+					setLoading(true);
+					setStatus(reviewResult);
+				} else Error(result.message);
+			});
+		setVisible(false);
+	};
 	// TODO: authorized people can review requisition
 	return (
 		<div>
 			<Button type="primary" onClick={showModal}>
 				View
 			</Button>
-			<Modal title="Requisition Details" visible={visible} onCancel={hideModal} footer={null}>
+			<Modal
+				title="Requisition Details"
+				visible={visible}
+				onCancel={hideModal}
+				footer={
+					status === "APPLIED"
+						? [
+								<Button
+									key="reject"
+									type="danger"
+									onClick={() => handleReview("REJECTED")}
+								>
+									Reject
+								</Button>,
+								<Button
+									key="approve"
+									type="primary"
+									onClick={() => handleReview("APPROVED")}
+								>
+									Approve
+								</Button>,
+						  ]
+						: null
+				}
+			>
 				<Descriptions>
 					<Descriptions.Item label="Collection Point">
 						{requisition.department.collectionPointId}
+					</Descriptions.Item>
+				</Descriptions>
+				<Descriptions>
+					<Descriptions.Item label="Requested By">
+						{requisition.requestedBy === null ? "" : requisition.requestedBy.name}
+					</Descriptions.Item>
+				</Descriptions>
+				<Descriptions>
+					<Descriptions.Item label="Requested Date">
+						{requisition.requestedOn}
 					</Descriptions.Item>
 				</Descriptions>
 				{status === "APPROVED" ? (
@@ -222,6 +283,16 @@ const RequisitionModal = ({ text }) => {
 					scroll={{ y: 400 }}
 					size="small"
 				/>
+				{status === "APPLIED" ? (
+					<>
+						<Divider dashed />
+						<Input
+							placeholder="Reject Reason (Optional)"
+							value={rejectReason}
+							onChange={(e) => setRejectReason(e.target.value)}
+						/>
+					</>
+				) : null}
 			</Modal>
 		</div>
 	);
