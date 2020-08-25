91c00beffb8e7a3586b6bc5d44f1aaa45235f333
Martini024 <zhao435021640@gmail.com>
2020-08-25 20:33:15 +0800

    fix supplier

--- ClientApp/src/page/component/Confirm.js
+++ ClientApp/src/page/component/Confirm.js
@@ -1,13 +1,12 @@
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 import { Modal } from "antd";
 import React from "react";
-const Confirm = (title, content, onOk, onCancel) => {
+const Confirm = (title, content, onOk) => {
 	Modal.confirm({
 		title: title,
 		icon: <ExclamationCircleOutlined />,
 		content: content,
 		onOk: onOk,
-		onCancel: onCancel,
 	});
 };
 export default Confirm;
